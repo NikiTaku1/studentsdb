2b90f38772ae9854907febfc2409e3668f5d61f0
NikiTaku1 <117597772+NikiTaku1@users.noreply.github.com>
2023-04-28 22:35:42 +0300

    Add files via upload

--- student database/dbgen.cpp
+++ student database/dbgen.cpp
@@ -8,7 +8,8 @@
 
 using namespace std;
 
-vector<string> faculties = {"amcp", "mathmech", "physfac", "chemfac"};
+vector<string> faculties = {"appliedmath", "mathmech", "physics", "chemistry", "programming"};
+vector<string> university = {"SPBU", "MSU", "PolyTech", "MIPT", "BaumanSU", "MISAA", "SPBSEU"};
 
 string RandomName(){
     int namesize = 735;
@@ -63,7 +64,7 @@ string RandomRoom(){
 string RandomCourse(string a){
     string asub = a.substr(a.size()-4, 4);
 
-    return to_string(2005-stoi(asub)) + '_' + (faculties[rand()%4]);
+    return to_string(2005-stoi(asub)) + '_' + (faculties[rand()%5]) + '_' + (university[rand()%7]);
 }
 class Student {
 private:
@@ -83,14 +84,14 @@ public:
 };
 
 int main() {
-//    setlocale(LC_ALL, "Russian");
-    SetConsoleCP(65001);// установка кодовой страницы win-cp 1251 в поток ввода
-    SetConsoleOutputCP(65001); // установка кодовой страницы win-cp 1251 в поток вывода
+//    setlocale(LC_ALL, "Russian") у меня почему-то не работает
+    SetConsoleCP(65001);
+    SetConsoleOutputCP(65001);
 
     unsigned seed = time(0);
     srand(seed);
 
-    int size; // Размер массива объектов класса
+    int size;
     cout << "Enter desired amount of students ";
     cin >> size;
     cout << "\n";
@@ -99,48 +100,32 @@ int main() {
     stdb.reserve(size);
     for (int i = 0; i < size; ++i) {
         string pname, pnum, pdate, proom, pcou_fac;
-//        cout << "Name: ";
         pname = RandomName();
-//        cout << "Number: ";
-//        pnum = to_string(i+1);
         pnum = RandomNumber(i+1, size);
-//        cout << "Date of birth: ";
         string a;
         a = RandomDate();
         pdate = a;
-//        cout << "Room_Dormitory: ";
         proom = RandomRoom();
-//        cout << "Course_Faculty: ";
         pcou_fac = RandomCourse(a);
 
-        Student p; // объявление объекта
+        Student p;
         p.setName(pname);
         p.setNumber(pnum);
-        pnum="";
         p.setDate(pdate);
         p.setRoom(proom);
         p.setCourseAndFaculty(pcou_fac);
 
         stdb.push_back(p);
-
-        cout << stdb[i].getName() << "\n";
+        cout << stdb[i].getName() << "\n"; //вывод каждой карточки
         cout << stdb[i].getNumber() << "\n";
         cout << stdb[i].getDate() << "\n";
         cout << stdb[i].getRoom() << "\n";
         cout << stdb[i].getCourseAndFaculty() << "\n";
         cout << "\n";
     }
-    
-//    cout << "Поиск по порядковому номеру:";
-//    int n;
-//    cin >> n;
-//    cout << stdb[n-1].getName() << "\n";
-//    cout << stdb[n-1].getNumber() << "\n";
-//    cout << stdb[n-1].getDate() << "\n";
-//    cout << stdb[n-1].getRoom() << "\n";
-//    cout << stdb[n-1].getCourseAndFaculty() << "\n";
-//    cout << "\n";
 
+    SetConsoleCP(866);
+    SetConsoleOutputCP(866);
     system("pause");
 
     return 0;
